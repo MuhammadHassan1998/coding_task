330440d90caaa0eab7d5639c94a64633df68608e
MuhammadHassan1998 <muhammadhhassan1998@gmail.com>
2023-10-02 20:41:22 -0400

    added models for the app

--- apps/book/models.py
+++ apps/book/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __repr__(self):
+        return self.title
+
+
+class Section(models.Model):
+    title = models.CharField(max_length=100)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    parent_section = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.CASCADE
+    )
+
+    def __repr__(self):
+        return self.title
+
+
+class Subsection(models.Model):
+    title = models.CharField(max_length=100)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    parent_subsection = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.CASCADE
+    )
+
+    def __repr__(self):
+        return self.title
+
+
+class Collaboration(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    is_author = models.BooleanField(default=False)
+
+    def __repr__(self):
+        return self.user.name
